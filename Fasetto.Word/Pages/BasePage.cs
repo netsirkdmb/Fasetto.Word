cb858e7fc2f7c8700d7e1f9af3e5602967d4cc7b
Kristen Dhuse <kristendhuse@gmail.com>
2018-02-07 17:02:44 -0800

    finishing page animations - video 7

---
+++ Fasetto.Word/Pages/BasePage.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// A base page for all pages to gain base functionality
+    /// </summary>
+    public class BasePage : Page
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The animation to play when the page is first loaded
+        /// </summary>
+        public PageAnimation PageLoadAnimation { get; set; } = PageAnimation.SlideAndFadeInFromRight;
+
+        /// <summary>
+        /// The animation to play when the page is unloaded
+        /// </summary>
+        public PageAnimation PageUnloadAnimation { get; set; } = PageAnimation.SlideAndFadeOutToLeft;
+
+        /// <summary>
+        /// The time any slide animation takes to complete
+        /// </summary>
+        public float SlideSeconds { get; set; } = 1.0f;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public BasePage()
+        {
+            // If we are animating in, hide to begin with
+            if (this.PageLoadAnimation != PageAnimation.None)
+            {
+                this.Visibility = Visibility.Collapsed;
+            }
+
+            // Listen out for the page loading
+            this.Loaded += BasePage_Loaded;
+        }
+
+        #endregion
+
+        #region Animation Load / Unload
+
+        /// <summary>
+        /// Once the page is loaded, performa any required animation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void BasePage_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Animate the page in
+            await AnimateIn();
+        }
+
+        /// <summary>
+        /// Animates in this page
+        /// </summary>
+        /// <returns></returns>
+        public async Task AnimateIn()
+        {
+            // Make sure we have something to do
+            if (this.PageLoadAnimation == PageAnimation.None)
+            {
+                return;
+            }
+
+            switch (this.PageLoadAnimation)
+            {
+                case PageAnimation.SlideAndFadeInFromRight:
+
+                    // Start the animation
+                    await this.SlideAndFadeInFromRight(this.SlideSeconds);
+
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Animates this page out
+        /// </summary>
+        /// <returns></returns>
+        public async Task AnimateOut()
+        {
+            // Make sure we have something to do
+            if (this.PageUnloadAnimation == PageAnimation.None)
+            {
+                return;
+            }
+
+            switch (this.PageUnloadAnimation)
+            {
+                case PageAnimation.SlideAndFadeOutToLeft:
+
+                    // Start the animation
+                    await this.SlideAndFadeOutToLeft(this.SlideSeconds);
+
+                    break;
+            }
+        }
+
+        #endregion
+    }
+}
